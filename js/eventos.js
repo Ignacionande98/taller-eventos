d39f0bbbbf8231d1b0b9c0429944255fa2491655
Ignacionande98 <128426701+Ignacionande98@users.noreply.github.com>
2023-08-07 19:22:27 -0300

    solucion del boton

--- js/eventos.js
+++ js/eventos.js
@@ -3,11 +3,15 @@
 document.addEventListener("DOMContentLoaded", function(){
     
     const divMensaje = document.querySelector("#divButton")
+    const button = document.querySelector("button")
 
-    if (divMensaje) {
-        divMensaje.addEventListener("click", function(){
+    if (divMensaje && button) {
+        divMensaje.addEventListener("click", function(event){
+            
             alert("Hola! , soy el div!")
+            event.stopPropagation()
         })
+        
     }
 })
 
